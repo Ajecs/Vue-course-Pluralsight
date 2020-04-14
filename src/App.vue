b46d1b524bd5c04666e2c983c2cd7629b9d5a383
Nicolas Coriale <nico7coriale@gmail.com>
2020-04-14 16:19:52 -0300

    Primer commit del curso Vue Fundamentals de PluralSight. Se agregó nombre y borro elementos de la App por defecto.

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <main>
+      <HomePage/>
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+main {
+  margin: 0 auto;
+  padding: 2em;
+  background-color:white;
+  width: 75vw;
+  min-height: 300px;
+  
+}
+</style>
